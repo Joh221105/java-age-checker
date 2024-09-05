be1a8b4f570b43ab5f01c0fa0ef00ae0a37351c1
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-09-04 20:32:02 -0400

    implements code to calculate the age of user and prints it out

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 package src;
+import java.time.Year;
 import java.util.Scanner;
-
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hi, what year were you born in?: ");
         int birthYear = scanner.nextInt();
+        int year = Year.now().getValue();
+        int age = year - birthYear;
+        System.out.print("I have calculated your age to be: " + age + " or " + (age-1));
 
     }
 }
