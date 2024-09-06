90069ac1a23f3fa997e204ed960e5cc2b412f201
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-09-06 10:00:56 -0400

    wraps user inputs in while loop to continuously ask for birth years until user quits

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 package src;
+
 import java.time.Year;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -6,32 +7,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hi, what year were you born in?: ");
-        try{
-            int birthYear = scanner.nextInt();
-            int age = calculateAge(birthYear);
-            displayResult(age);
-        }
-        catch(InputMismatchException e){
-            System.out.print("Please enter a number for your age");
-        }
+        boolean continueRunning = true;
 
+        while (continueRunning) {
+            System.out.println("Hi, what year were you born in? (or type 'exit' to quit): ");
+            try {
+                if (scanner.hasNext("exit")) {
+                    continueRunning = false;
+                    break;
+                }
+
+                int birthYear = scanner.nextInt();
+                int age = calculateAge(birthYear);
+                displayResult(age);
+
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid number for your birth year.");
+                scanner.nextLine();
+            }
+        }
+        scanner.close();
     }
 
-    public static int calculateAge(int birthYear){
+    public static int calculateAge(int birthYear) {
         int year = Year.now().getValue();
         return year - birthYear;
     }
 
-    public static void displayResult(int age){
-        if (age > 125){
-            System.out.print("Are you sure you typed in your year of birth correctly?");
-        }
-        else if (age < 0){
-            System.out.print("Please enter a valid age");
-        }
-        else{
-            System.out.print("I have calculated your age to be: " + age + " or " + (age-1));
+    public static void displayResult(int age) {
+        if (age > 125) {
+            System.out.println("Are you sure you typed in your year of birth correctly?");
+        } else if (age < 0) {
+            System.out.println("Please enter a valid age.");
+        } else {
+            System.out.println("I have calculated your age to be: " + age + " or " + (age - 1));
         }
     }
 }
