77b6d623ded8a4363e86dd8d282e738b0cc47748
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-09-05 20:56:17 -0400

    adds try catch block for non numeric inputs for scanner, adds validation for years entered that are greater than current year

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
 package src;
 import java.time.Year;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hi, what year were you born in?: ");
-        int birthYear = scanner.nextInt();
-        int age = calculateAge(birthYear);
-        if (age > 125){
-            System.out.print("Aer you sure you typed in your year of birth correctly?");
+        try{
+            int birthYear = scanner.nextInt();
+            int age = calculateAge(birthYear);
+            if (age > 125){
+                System.out.print("Are you sure you typed in your year of birth correctly?");
+            }
+            else if (age < 0){
+                System.out.print("Please enter a valid age");
+            }
+            else{
+                System.out.print("I have calculated your age to be: " + age + " or " + (age-1));
+            }
         }
-        else{
-            System.out.print("I have calculated your age to be: " + age + " or " + (age-1));
+        catch(InputMismatchException e){
+            System.out.print("Please enter a number for your age");
         }
+
     }
 
     public static int calculateAge(int birthYear){
