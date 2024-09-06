50fb4b301118ad678f474cf391b054dc761366b2
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-09-05 20:46:10 -0400

    creates separate function to calculate age

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hi, what year were you born in?: ");
         int birthYear = scanner.nextInt();
-        int year = Year.now().getValue();
-        int age = year - birthYear;
+        int age = calculateAge(birthYear);
         if (age > 125){
             System.out.print("Aer you sure you typed in your year of birth correctly?");
         }
         else{
             System.out.print("I have calculated your age to be: " + age + " or " + (age-1));
         }
+    }
+
+    public static int calculateAge(int birthYear){
+        int year = Year.now().getValue();
+        return year - birthYear;
     }
 }
