cb8b42dddbd1245d749b485d222e82851e76c544
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-09-08 20:12:42 -0400

    adds new boolean variable to track if user's birthday has passed and sends it to displayResult

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean birthdayPassed = true;
 
         while (true) {
             System.out.println("Hi, what year were you born in? (or type 'exit' to quit): ");
@@ -18,7 +19,7 @@ public class Main {
 
                 int birthYear = scanner.nextInt();
                 int age = calculateAge(birthYear);
-                displayResult(age);
+                displayResult(age, birthdayPassed);
 
             } catch (InputMismatchException e) {
                 System.out.println("Please enter a valid number for your birth year.");
@@ -33,13 +34,18 @@ public class Main {
         return year - birthYear;
     }
 
-    public static void displayResult(int age) {
+    public static void displayResult(int age, boolean birthdayPassed) {
         if (age > 125) {
             System.out.println("Are you sure you typed in your year of birth correctly?");
         } else if (age < 0) {
             System.out.println("Please enter a valid age.");
         } else {
-            System.out.println("I have calculated your age to be: " + age + " or " + (age - 1));
+            if (birthdayPassed){
+                System.out.println("I have calculated your age to be: " + age + " or " + (age - 1));
+            }
+            else{
+                System.out.println("I have calculated your age to be: " + age + " or " + (age - 1));
+            }
         }
     }
 }
